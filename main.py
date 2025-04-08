426b67e8273e31a22946c807a1f8438f9228b175
Paul Fouquet <Skypol59@msn.com>
2025-04-08 11:30:40 +0200

    incorporated the display

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
-print(chosen_word)
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter : ").lower()
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+print("debug:",chosen_word)
+
+placeholder = ["_" for symbol in chosen_word ] # EXPRESSION for ITEM in ITERABLE
+print ("".join(placeholder))
+
+guess = input("Guess a letter: ").lower()
+
+
+display = []
+for letter in chosen_word:
+    if letter == guess: # == means trully identical
+        display.append(letter) #j'injecte la lettre correcte dans l'array display
+    else:
+        display.append("_")
+
+print("".join(display))
+
 for letter in chosen_word:
     if letter == guess:
-        print ("Right")
+        print("Right")
     else:
-        print ("Wrong")
+        print("Wrong")
